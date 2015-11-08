252b6344155b98409ade156d25240a3d7c31cee1
Jesse Dovi <jdovi@drydenengineering.com>
2015-11-07 19:25:30 -0500

    added initial views.  Need to reconfig and add actions

--- sourcing/sourcetrack/views.py
+++ sourcing/sourcetrack/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+	return HttpResponse("Hello, world. You are at the sourcing index")
+	
+def detail(request, interview_id):
+	return HttpResponse("You're looking at question %s." % interview_id)
+	
+def interview(request, interview_id):
+	return HttpResponse("You're entering and interview %s." % interview_id)
