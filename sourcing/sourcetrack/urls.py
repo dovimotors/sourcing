1f11d519b46a718be092f0980b06f47fa08bde53
Jesse Dovi <jdovi@drydenengineering.com>
2015-11-08 19:45:55 -0500

    added sourcetrack.html and updated views.py

--- sourcing/sourcetrack/urls.py
+++ sourcing/sourcetrack/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^(?P<interview_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^interview/$', views.interview, name='interview'),
 	]
--- sourcing/sourcetrack/views.py
+++ sourcing/sourcetrack/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Interview
+from django.template import RequestContext, loader
 
 def index(request):
-	return HttpResponse("Hello, world. You are at the sourcing index")
+	latest_interview_list = Interview.objects.order_by('-int_date')[:5]
+	context = {'latest_interview_list': latest_interview_list}
+	return render(request, 'sourcetrack/index.html', context)
 	
 def detail(request, interview_id):
 	return HttpResponse("You're looking at question %s." % interview_id)
 	
-def interview(request, interview_id):
-	return HttpResponse("You're entering and interview %s." % interview_id)
+def interview(request):
+	return HttpResponse("You're entering and interview.")
